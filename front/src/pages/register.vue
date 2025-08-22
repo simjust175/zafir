9454cdadd572f17bb91880a6063409c60e1379c8
simjust175 <bunim175@gmail.com>
2025-08-22 16:56:10 +0200

    removed test from front-end

--- front/src/pages/register.vue
+++ front/src/pages/register.vue
@@ -3,8 +3,6 @@
     <main
       class="pa-0"
     >
-    <v-card color="blue">{{ db || "EMPTY 🥲😢😭" }}</v-card>
-    <v-btn color="red" @click="connectToDb">TEST TEST!</v-btn>
       <!-- <v-btn icon="mdi-close" size="x-large" variant="plain" color="white" @click="router.push('/')"></v-btn> -->
       <RegisterMain />
     </main>
@@ -15,14 +13,6 @@
 import { useTheme } from 'vuetify';
 import { useRouter } from 'vue-router';
 import RegisterMain from '@/components/RegisterForms/RegisterMain.vue';
-
-//test test tes 
-import { ref } from "vue"
-let db = ref("")
-const connectToDb = async ()=> {
-  const res = await fetch(`${import.meta.env.VITE_BASE_URL}/invoice/freeEmails`)
-  db.value =  await res.json()
-}
 
 const router = useRouter();
 const theme = useTheme();
