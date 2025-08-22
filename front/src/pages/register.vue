058b6713bc5fb7fbf6831b7b0c41e369d3009f2d
simjust175 <bunim175@gmail.com>
2025-08-22 11:54:52 +0200

    test

--- front/src/pages/register.vue
+++ front/src/pages/register.vue
@@ -3,6 +3,8 @@
     <main
       class="pa-0"
     >
+    <v-card color="blue">{{ db || "EMPTY 🥲😢😭" }}</v-card>
+    <v-btn color="red" @click="connectToDb">TEST TEST!</v-btn>
       <!-- <v-btn icon="mdi-close" size="x-large" variant="plain" color="white" @click="router.push('/')"></v-btn> -->
       <RegisterMain />
     </main>
@@ -14,6 +16,13 @@ import { useTheme } from 'vuetify';
 import { useRouter } from 'vue-router';
 import RegisterMain from '@/components/RegisterForms/RegisterMain.vue';
 
+//test test tes 
+import { ref } from "vue"
+let db = ref("")
+const connectToDb = async ()=> {
+  const res = await fetch(`${import.meta.env.VITE_BASE_URL}/invoice/freeEmails`)
+  db.value =  await res.json()
+}
 
 const router = useRouter();
 const theme = useTheme();
