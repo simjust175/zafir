d5564f40994eeb9f766b8e77ba797c8d34fab5ff
simjust175 <bunim175@gmail.com>
2026-01-26 12:21:10 +0100

    switched to Resend system

--- Server/Login_system/invite_email/sendInviteEmail.js
+++ Server/Login_system/invite_email/sendInviteEmail.js
@@ -1,5 +1,9 @@
-import transporter from "../reset_email/mailer.js";
+// import transporter from "../reset_email/mailer.js";
+import { Resend } from "resend"
 import { inviteEmailTemplate } from "./inviteEmailTemplate.js";
+
+// ✅ Initialize Resend
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function sendInviteEmail({ email, name, token, expiresAt }) {
   const inviteLink = `${process.env.FRONTEND_URL}/accept-invite?token=${token}`;
@@ -11,10 +15,12 @@ export async function sendInviteEmail({ email, name, token, expiresAt }) {
     expiresAt
   });
 
-  await transporter.sendMail({
+  //await transporter.sendMail({
+  await resend.emails.send({
     from: `"${process.env.APP_NAME || "Billio"}" <${process.env.RESET_EMAIL_USER}>`,
     to: email,
     subject: `You're invited to join ${process.env.APP_NAME || "Billio"}`,
     html
   });
 }
+
