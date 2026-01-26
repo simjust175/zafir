c7a27b1f8bba6f1eef5550d0132f1ec76bc37d19
simjust175 <bunim175@gmail.com>
2026-01-26 13:34:56 +0100

    added upload page

---
+++ front/src/pages/upload.vue
@@ -0,0 +1,19 @@
+<template>
+  <main>
+    <invoice-upload-main
+      :active="activateDialog"
+      @close="$router.push('/invoices')"
+    />
+  </main>
+</template>
+
+<script setup>
+import InvoiceUploadMain from '@/components/InvoiceUpload/InvoiceUploadMain.vue';
+import { onMounted, ref } from "vue";
+const activateDialog = ref(false)
+onMounted(()=> activateDialog.value = true)
+</script>
+
+<style>
+
+</style>
